5c3b939fc74854611da748de9787ca82fdbad854
MitulIslam <mi@inov.io>
2017-04-17 16:24:57 +0600

    extractor refractor

--- extract_from_dcm.py
+++ extract_from_dcm.py
@@ -2,31 +2,22 @@ import dicom
 import os
 import csv
 
-directory_data = os.listdir("dicom")
-print type(directory_data)
-print directory_data
+dicom_dir_name = os.listdir("dicom")
+print type(dicom_dir_name)
+print dicom_dir_name
 
-for data in directory_data:
-    dicom_data = dicom.read_file("dicom/" + data)
-    # print "----------------------- dicom data -----------------------"
-    print dicom_data.dir()
+for dicom_file_name in dicom_dir_name:
+    dicom_data = dicom.read_file("dicom/" + dicom_file_name)
+    print "----------------------- dicom data -----------------------"
+    # print dicom_data.dir()
     dicom_tags = dicom_data.dir()
+    os.system("cd dicom && time dcm2pnm "  + dicom_file_name + " ../image/" + dicom_file_name[:-4]+".pnm -v")
+    os.system("cd image && time flif " + dicom_file_name[:-4] + ".pnm ../flif/" + dicom_file_name[:-4]+".flif -V")
 
-    # file= open("data/"+data[:-4] + ".csv","wb")
-    # writer = csv.writer(file, dialect='excel')
-    # for row in dicom_data:
-    #     with file
-        # writer.writerow(row)
-    with open("data/"+str(data[:-4]) + ".csv", "wb") as data_file:
+    with open("data/"+str(dicom_file_name[:-4]) + ".csv", "wb") as data_file:
         for dicom_tag in dicom_tags:
             if dicom_tag != "PixelData" and dicom_tag != "SourceImageSequence":
-                print str(dicom_data.ImageType).replace(",", "/")
+                # print str(dicom_data.ImageType).replace(",", "/")
                 refined_data = str(getattr(dicom_data, str(dicom_tag))).replace(",", "/")
                 writer = csv.writer(data_file, delimiter='|', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
                 writer.writerow([dicom_tag, str(refined_data) if refined_data else None])
-
-        # file.close()
-    # with open("data/"+data[:-4] + '.csv', 'wb') as csvfile:
-    #     spamwriter = csv.writer(csvfile, delimiter=' ', quotechar=',', quoting=csv.QUOTE_MINIMAL)
-    #     spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
-    #     spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
