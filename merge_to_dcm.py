e5223f3c725f1233535003f20a4cc54556edf4b0
MitulIslam <mi@inov.io>
2017-04-17 14:59:47 +0600

    merge pnm and data

--- merge_to_dcm.py
+++ merge_to_dcm.py
@@ -3,8 +3,10 @@ import os
 import csv
 
 directory_data = os.listdir("data/")
-# print type(directory_data)
+dicom_dir = os.listdir("image/")
+print dicom_dir
 print directory_data
+dicom_file = []
 
 # f = open('attendees1.csv')
 # csv_f = csv.reader(f)
@@ -14,21 +16,47 @@ print directory_data
 #
 # for row in csv_f:
 #     print row
+for dicoms in dicom_dir:
+    if str(dicoms).endswith(".dcm"):
+        dicom_file.append(dicoms[:-4])
+print dicom_file
 
 for csvfile in directory_data:
-    f = open("data/" + str(csvfile), "rb")
-    csv_f = csv.reader(f)
+    # f = open("data/" + str(csvfile), "rb")
+    # csv_f = csv.reader(f)
+    #
+    # print "--------------------------------csv data--------------------------------"
+    # for row in csv_f:
+    #     print row[0]
+    #
+    # f.close()
+    if csvfile[:-4] in dicom_file:
+        dcm_file_path = "image/" + str(csvfile[:-4]) + ".dcm"
+        read_dicom_file = dicom.read_file(dcm_file_path)
+        all_tags = read_dicom_file.dir()
+        print str(read_dicom_file.dir())
+        with open("data/" + str(csvfile), "rb") as datafile:
+            data_reader = csv.reader(datafile, delimiter='|', quotechar=' ')
+            # print data_reader
+            print "--------------------------------csv data--------------------------------"
+            for row in data_reader:
+                if(row[0] in all_tags):
+                    print 'dcmodify -m "%s=%s" %s' % (str(row[0]), str(row[1]), dcm_file_path)
+                    try:
+                        dcmmod = os.system('dcmodify -m "%s=%s" %s -v' % (str(row[0]), str(row[1]), dcm_file_path))
+                        print "Dicom modiy code: " + str(dcmmod)
+                        print "Existing tags " + str(row[0]) + " , " + str(row[1])
+                    except Exception as e:
+                        print "Cannot modify dicom. Reason: " + str(e)
+                else:
+                    print 'dcmodify -i "%s=%s" %s' % (str(row[0]), str(row[1]), dcm_file_path)
+                    try:
+                        dcmmod = os.system('dcmodify -i "%s=%s" %s -v' % (str(row[0]), str(row[1]), dcm_file_path))
+                        print "Dicom insert code: " + str(dcmmod)
+                        print "New Tags " + str(row[0]) + " , " + str(row[1])
+                    except Exception as e:
+                        print "Cannot insert new tag to dicom. Reason: " + str(e)
 
-    print "--------------------------------csv data--------------------------------"
-    for row in csv_f:
-        print row[0]
-
-    f.close()
-    # with open("data/" + str(csvfile), "rb") as datafile:
-    #     data_reader = csv.reader(datafile, delimiter='|', quotechar=' ')
-    #     print data_reader
-    #     for row in data_reader:
-    #         print row
 
 # for data in directory_data:
 #     with open("data/" + str(data[:-4]) + ".csv", "r") as data_file:
